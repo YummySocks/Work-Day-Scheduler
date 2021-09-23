228a6762f252fe562c8e19b41da5a7dd0f4a740e
Socks <patricksutcliffe123@gmail.com>
2021-09-23 14:01:56 -0400

    got it to dynamically change colors based on current time

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do"));
+var middleBlock = $('textarea');
+var nine = moment(9, "H").isBefore();
+var ten = moment(10, "H").isBefore();
+var eleven = moment(11, "H").isBefore();
+var twelve = moment(12, "H").isBefore();
+var one = moment(13, "H").isBefore();
+var two = moment(14, "H").isBefore();
+var three = moment(15, "H").isBefore();
+var four = moment(16, "H").isBefore();
+var five = moment(17, "H").isBefore();
+var times = [nine, ten, eleven, twelve, one, two, three, four, five]
+console.log(times);
+
+$("i").on("click",function(event){
+    console.log(event.target);
+})
+var hourNow = moment().format("h");
+console.log(hourNow);
+
+var hoursInDay = $('span')
+console.log(hoursInDay[0].innerHTML)
+
+middleBlock.each( function(i){
+    tempT = times[i];
+    tempH = hoursInDay[i].innerHTML
+    if (tempH == hourNow ){
+        $(this).toggleClass("present");
+    } else if (tempT){
+        $(this).toggleClass("past");
+    } else {
+        $(this).toggleClass("future")
+    }
+    
+
+})
