2984644891b1b404dcfa046c5b1bc36828f968ae
Socks <patricksutcliffe123@gmail.com>
2021-09-23 21:17:33 -0400

    finished commenting out the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+// setting the current day into the p tag in the header
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 var middleBlock = $('textarea');
+// getting the boolean of whether or not the current time is before each hour
 var nine = moment(9, "H").isBefore();
 var ten = moment(10, "H").isBefore();
 var eleven = moment(11, "H").isBefore();
@@ -10,14 +12,13 @@ var two = moment(14, "H").isBefore();
 var three = moment(15, "H").isBefore();
 var four = moment(16, "H").isBefore();
 var five = moment(17, "H").isBefore();
-var times = [nine, ten, eleven, twelve, one, two, three, four, five]
-console.log(times);
-var storage = [];
+// stores those booleans into an array
+var times = [nine, ten, eleven, twelve, one, two, three, four, five];
+// adds the event listener to that links to storing the values
 $(".saveBtn").on("click",storeValues);
+// pulls the current hour based on right now
 var hourNow = moment().format("H");
-console.log(hourNow);
-var hoursInDay = $('span')
-console.log(hoursInDay[0].innerHTML)
+// iterates through each textarea element and sets the class depending on current time
 middleBlock.each( function(i){
     tempT = times[i];
     tempH = $(this).parent().attr('id');
@@ -26,12 +27,14 @@ middleBlock.each( function(i){
     } else if (tempT){
         $(this).toggleClass("past");
     } else {
-        $(this).toggleClass("future")
+        $(this).toggleClass("future");
     }
-    $(this).val(localStorage.getItem(tempH))  
+    // writes the stored values back into the text area based on its key
+    $(this).val(localStorage.getItem(tempH));  
 })
+// saves each text area input based on its position on the page
 function storeValues(){
     var input = $(this).siblings("textarea").val().trim();
-    var valueId = $(this).parent().attr('id')
-    localStorage.setItem(valueId, input)
+    var valueId = $(this).parent().attr('id');
+    localStorage.setItem(valueId, input);
 }
